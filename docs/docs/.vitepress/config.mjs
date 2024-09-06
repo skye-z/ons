4f7fcb33e9eaac1302275cb26ab25873875ea35b
skay <skai-zhang@hotmail.com>
2024-09-06 19:28:22 +0800

    0.1.3

---
+++ docs/docs/.vitepress/config.mjs
@@ -0,0 +1,74 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  base: "/nas-sync/",
+  title: "BetaX ONS",
+  description: "BetaX Obsidian NAS Sync Server",
+  head: [['link', { rel: 'icon', href: '/icon/icon-light@1x.png' }]],
+  themeConfig: {
+    logo: '/icon/icon-light@2x.png',
+    nav: [
+      { text: '指南', link: '/guide/about' },
+      { text: '插件', link: '/guide/about' },
+      {
+        text: '服务', items: [
+          { text: 'NAS服务', link: '/guide/about' },
+          { text: '中控服务', link: '/guide/about' }
+        ]
+      },
+    ],
+    sidebar: [
+      {
+        text: '指南',
+        items: [
+          { text: '关于 ONS', link: '/guide/about' },
+          { text: '快速开始', link: '/guide/start' },
+          { text: '兼容性', link: '/guide/compatible' },
+          { text: '初始化', link: '/guide/reinit' },
+          { text: '最小化', link: '/guide/min' }
+        ]
+      },
+      {
+        text: 'Obsidian',
+        items: [
+          { text: '基本配置', link: '/guide/about' },
+          { text: '常见问题', link: '/guide/about' },
+        ]
+      },
+      {
+        text: 'NAS 服务',
+        items: [
+          { text: '注册设备', link: '/guide/about' },
+          { text: '同步控制', link: '/guide/about' },
+          { text: '连接密码', link: '/guide/about' },
+        ]
+      },
+      {
+        text: '中控服务',
+        items: [
+          { text: '注册登录', link: '/guide/about' },
+          { text: '设备管理', link: '/guide/about' },
+          { text: '自行部署', link: '/guide/about' },
+        ]
+      }
+    ],
+    outline: {
+      level: [2, 6]
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/skye-z/nas-sync' }
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2024-present Skye Zhang'
+    },
+    search: {
+      provider: 'local'
+    },
+  },
+  markdown: {
+    image: {
+      lazyLoading: true
+    }
+  }
+})
---
+++ docs/docs/index.md
@@ -0,0 +1,49 @@
+---
+layout: home
+
+hero:
+  name: "BetaX"
+  text: "Obsidian\nNAS Sync Server"
+  tagline: 让 Obsidian 与你家中内网的 NAS 时刻保持同步
+  image:
+    src: /icon/icon-dark@4x.png
+    alt: VitePress
+  actions:
+    - theme: brand
+      text: 快速开始
+      link: /guide/start
+    - theme: alt
+      text: Github
+      link: https://github.com/skye-z/nas-sync
+
+features:
+  - icon: 💰
+    title: 成本少
+    details: 采用 P2P 技术实现 Obsidian 客户端与 NAS 端的点对点传输, 仅自行部署云端中控服务器存在成本
+  - icon: 🚀
+    title: 传输快
+    details: 当通过中控服务器建立连接后, 点对点传输将得同步服务无需再进行中转, 可以最大化利用你的带宽
+  - icon: ⚡️
+    title: 开销低
+    details: 闲置时仅 ~3% CPU 和 ~20MB 内存开销, 在传输 100MB 文件时也仅 3 ~ 8% CPU 和 ~50MB 的内存开销
+  - icon: 🧠
+    title: 易上手
+    details: 提供简单易用的可视化界面, 大幅精简必要的操作步骤, 以及完善的使用文档, 都可以帮助你快速上手
+---
+
+<style>
+  :root {
+    --vp-button-brand-bg: #08BDC9;
+    --vp-button-brand-hover-bg: #25D8E4;
+
+    --vp-home-hero-name-color: transparent;
+    --vp-home-hero-name-background: -webkit-linear-gradient(120deg, #bd34fe 30%, #08BDC9 70%);
+
+    --vp-home-hero-image-filter: blur(68px);
+    --vp-home-hero-image-background-image: linear-gradient(-45deg, #bd34fe 30%, #08BDC9 70%);
+  }
+
+  .VPHome{
+    padding-bottom: 30px !important;
+  }
+</style>
