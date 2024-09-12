72bf73782b139ce65f401b864e4b9f0a6dfacc18
skay <skai-zhang@hotmail.com>
2024-09-12 12:33:43 +0800

    1.0.0

--- obsidian-plugin/src/setting.ts
+++ obsidian-plugin/src/setting.ts
@@ -15,8 +15,8 @@ export class NSSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('中控服务器')
-			.setDesc('中控负责登记 NAS 设备, 提供鉴权并促成连接的建立')
+			.setName('Central control server')
+			.setDesc('中控服务器')
 			.addText(text => text
 				.setPlaceholder('ons.betax.dev')
 				.setValue(this.plugin.settings.server)
@@ -25,8 +25,8 @@ export class NSSettingTab extends PluginSettingTab {
 					await this.plugin.saveSettings();
 				}));
 		new Setting(containerEl)
-			.setName('唯一标识')
-			.setDesc('让 Obsidian 能在中控服务器中找到你的 NAS 的唯一标识')
+			.setName('NAT.ID')
+			.setDesc('唯一标识')
 			.addText(text => text
 				.setPlaceholder('000000')
 				.setValue(this.plugin.settings.devId)
@@ -35,8 +35,8 @@ export class NSSettingTab extends PluginSettingTab {
 					await this.plugin.saveSettings();
 				}));
 		new Setting(containerEl)
-			.setName('连接密码')
-			.setDesc('NAS 中的设置的密码, NAS 会判断密码来决定是否建立连接')
+			.setName('Connection password')
+			.setDesc('连接密码')
 			.addText(text => text
 				.setPlaceholder('8-24位')
 				.setValue(this.plugin.settings.pwd)
@@ -45,10 +45,10 @@ export class NSSettingTab extends PluginSettingTab {
 					await this.plugin.saveSettings();
 				}));
 		new Setting(containerEl)
-			.setName('连接测试')
-			.setDesc('配置完成后可使用此功能测试配置是否正确')
+			.setName('Connection test')
+			.setDesc('连接测试')
 			.addButton(text => text
-				.setButtonText("开始测试")
+				.setButtonText("Start")
 				.onClick(async () => {
 					new Notice("开始测试, 请留意右上角提示与右下角的状态")
 					this.plugin.initPeerManager();
